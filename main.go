8af0e319445f12036e53063f2cc61a65fe1bb9ca
bom_d_van <bom.d.van@gmail.com>
2015-06-28 20:46:10 +0800

    double trigger to kill old say process

--- main.go
+++ main.go
@@ -4,25 +4,68 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
 func main() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pidFile := filepath.Join(usr.HomeDir, ".gosay-say-pid")
+	log.SetFlags(log.Lshortfile)
+	pidStr, err := ioutil.ReadFile(pidFile)
+	if err != nil {
+		log.Println(err)
+	} else if pid, err := strconv.Atoi(string(pidStr)); err == nil {
+		ps, err := os.FindProcess(int(pid))
+		if err != nil {
+			log.Println(err)
+		} else if err := ps.Signal(syscall.Signal(0)); err == nil {
+			if err := ps.Kill(); err != nil {
+				log.Println(err)
+			} else {
+				return
+			}
+		} else {
+			// log.Println(err)
+		}
+	} else {
+		log.Println(err)
+	}
+
 	if len(os.Args) == 1 {
 		println("expect input")
 		os.Exit(1)
 	}
-	words := os.Args[1]
-	var output []byte
-	var err error
+	words := strings.Join(os.Args[1:], " ")
 	c := []rune(words)[0]
+	var args []string
 	if 0 <= c && c <= 127 {
-		output, err = exec.Command("say", words).CombinedOutput()
+		args = []string{words}
 	} else {
-		output, err = exec.Command("say", "-v", "Otoya", words).CombinedOutput()
+		args = []string{"-v", "Otoya", words}
 	}
+	cmd := exec.Command("say", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		log.Printf("error: %s\n", err)
+		os.Exit(1)
+	}
+	file, err := os.Create(pidFile)
 	if err != nil {
-		fmt.Printf("error (%s): %s\n", err, output)
+		log.Println(err)
+		os.Exit(1)
 	}
+	file.WriteString(fmt.Sprint(cmd.Process.Pid))
+	file.Close()
 }
