69f72eadf69e38aff86f4793d3f2cb60cd60c4d4
bom_d_van <bom.d.van@gmail.com>
2015-06-28 20:01:04 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		println("expect input")
+		os.Exit(1)
+	}
+	words := os.Args[1]
+	var output []byte
+	var err error
+	c := []rune(words)[0]
+	if 0 <= c && c <= 127 {
+		output, err = exec.Command("say", words).CombinedOutput()
+	} else {
+		output, err = exec.Command("say", "-v", "Otoya", words).CombinedOutput()
+	}
+	if err != nil {
+		fmt.Printf("error (%s): %s\n", err, output)
+	}
+}
